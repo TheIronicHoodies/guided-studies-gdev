f5a3483a05a4ad4472c28dbc29a8cc82ddd728ff
Miggy Go <miguelivann1129@gmail.com>
2025-08-25 14:17:08 +0800

    Made framework code for detecting Tile Type, added static fields to player movement (needs tile sprites to properly test)

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -5,21 +5,27 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 5f;
     public Transform movePoint;
     public LayerMask borderCheck;
-    public LayerMask inputCheck;
+    public static bool inputCheck, arrived;
+    public static float direction;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movePoint.parent = null;
+        inputCheck = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(transform.position, movePoint.position) == 0f)
+        //variable to check if Player and Movement point at the same point
+        arrived = (Vector3.Distance(transform.position, movePoint.position) == 0f);
+
+        if (arrived && (inputCheck))
         {
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
+                direction = Input.GetAxisRaw("Horizontal");
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, borderCheck))
                 {
                     movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
@@ -27,6 +33,7 @@ public class PlayerController : MonoBehaviour
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
+                direction = Input.GetAxisRaw("Vertical") * 2;
                 if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, borderCheck))
                 {
                     movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
