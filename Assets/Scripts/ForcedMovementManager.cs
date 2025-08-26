65fed3c6f5943d24e8fe6a52332297accad58674
evil levi <127681855+TheIronicHoodies@users.noreply.github.com>
2025-08-26 11:39:46 +0800

    debugged

--- Assets/Scripts/ForcedMovementManager.cs
+++ Assets/Scripts/ForcedMovementManager.cs
@@ -1,156 +1,156 @@
-using NUnit.Framework;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Tilemaps;
+// using NUnit.Framework;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.Tilemaps;
 
-public class ForcedMovementManager : MonoBehaviour
-{
-    public Transform player;
-    public Transform movePoint;
-    public LayerMask borderCheck;
+// public class ForcedMovementManager : MonoBehaviour
+// {
+//     public Transform player;
+//     public Transform movePoint;
+//     public LayerMask borderCheck;
 
-    [SerializeField]
-    private Tilemap map;
+//     [SerializeField]
+//     private Tilemap map;
 
-    [SerializeField]
-    private List<TileBehaviour> tileTypes;
+//     [SerializeField]
+//     private List<TileBehaviour> tileTypes;
 
-    private Dictionary<TileBase, TileBehaviour> dataFromTiles;
+//     private Dictionary<TileBase, TileBehaviour> dataFromTiles;
 
-    private void Awake()
-    {
-        dataFromTiles = new Dictionary<TileBase, TileBehaviour>();
+//     private void Awake()
+//     {
+//         dataFromTiles = new Dictionary<TileBase, TileBehaviour>();
 
-        foreach (var tileBehaviour in tileTypes)
-        {
-            foreach (var tile in TileBehaviour.tiles)
-            {
-                dataFromTiles.Add(tile, tileBehaviour);
-            }
-        }
-    }
+//         foreach (var tileBehaviour in tileTypes)
+//         {
+//             foreach (var tile in TileBehaviour.tiles)
+//             {
+//                 dataFromTiles.Add(tile, tileBehaviour);
+//             }
+//         }
+//     }
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+//     // Start is called once before the first execution of Update after the MonoBehaviour is created
+//     void Start()
+//     {
         
-    }
+//     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3Int gridcoordinate = map.WorldToCell(player.position);
-        TileBase currentTile = map.GetTile(gridcoordinate);
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         Vector3Int gridcoordinate = map.WorldToCell(player.position);
+//         TileBase currentTile = map.GetTile(gridcoordinate);
 
-        int tileDirection = dataFromTiles[currentTile].direction;
+//         int tileDirection = dataFromTiles[currentTile].direction;
 
-        if (PlayerController.arrived)
-        {
-            switch (tileDirection)
-            {
-
-
-                // Down Conveyor Tile Case
-                case -2:
-                    PlayerController.direction = = tileDirection;
-                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .2f, borderCheck))
-                    {
-                        PlayerController.inputCheck = false;
-                        movePoint.position += new Vector3(0f, -1f, 0f);
-                    }
-                    else
-                    {
-                        PlayerController.inputCheck = true;
-                    }
-                    break;
+//         if (PlayerController.arrived)
+//         {
+//             switch (tileDirection)
+//             {
 
 
-                //Left Conveyor Tile Case
-                case -1:
-                    PlayerController.direction = = tileDirection;
-                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-1f, 0f, 0f), .2f, borderCheck))
-                    {
-                        PlayerController.inputCheck = false;
-                        movePoint.position += new Vector3(-1f, 0f, 0f);
-                    }
-                    else
-                    {
-                        PlayerController.inputCheck = true;
-                    }
-                    break;
+//                 // Down Conveyor Tile Case
+//                 case -2:
+//                     PlayerController.direction = tileDirection;
+//                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, -1f, 0f), .2f, borderCheck))
+//                     {
+//                         PlayerController.inputCheck = false;
+//                         movePoint.position += new Vector3(0f, -1f, 0f);
+//                     }
+//                     else
+//                     {
+//                         PlayerController.inputCheck = true;
+//                     }
+//                     break;
 
 
-                // Slide Tile case
-                case 0:
-                    switch (PlayerController.direction)
-                    {
-                        case -2:
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, (PlayerController.direction) / 2, 0f), .2f, borderCheck))
-                            {
-                                movePoint.position += new Vector3(0f, (PlayerController.direction) / 2, 0f);
-                            }
-                            break;
-                        case -1:
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3((PlayerController.direction), 0f, 0f), .2f, borderCheck))
-                            {
-                                movePoint.position += new Vector3(PlayerController.direction, 0f, 0f);
-                            }
-                            break;
-                        case 1:
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3((PlayerController.direction), 0f, 0f), .2f, borderCheck))
-                            {
-                                movePoint.position += new Vector3(PlayerController.direction, 0f, 0f);
-                            }
-                            break;
-                        case 2:
-                            if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, (PlayerController.direction) / 2, 0f), .2f, borderCheck))
-                            {
-                                movePoint.position += new Vector3(0f, (PlayerController.direction) / 2, 0f);
-                            }
-                            break;
-                    }
-                    break;
+//                 //Left Conveyor Tile Case
+//                 case -1:
+//                     PlayerController.direction = tileDirection;
+//                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(-1f, 0f, 0f), .2f, borderCheck))
+//                     {
+//                         PlayerController.inputCheck = false;
+//                         movePoint.position += new Vector3(-1f, 0f, 0f);
+//                     }
+//                     else
+//                     {
+//                         PlayerController.inputCheck = true;
+//                     }
+//                     break;
 
 
-                // Right Conveyor Tile Case
-                case 1:
-                    PlayerController.direction = = tileDirection;
-                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(1f, 0f, 0f), .2f, borderCheck))
-                    {
-                        PlayerController.inputCheck = false;
-                        movePoint.position += new Vector3(1f, 0f, 0f);
-                    }
-                    else
-                    {
-                        PlayerController.inputCheck = true;
-                    }
-                    break;
+//                 // Slide Tile case
+//                 case 0:
+//                     switch (PlayerController.direction)
+//                     {
+//                         case -2:
+//                             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, (PlayerController.direction) / 2, 0f), .2f, borderCheck))
+//                             {
+//                                 movePoint.position += new Vector3(0f, (PlayerController.direction) / 2, 0f);
+//                             }
+//                             break;
+//                         case -1:
+//                             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3((PlayerController.direction), 0f, 0f), .2f, borderCheck))
+//                             {
+//                                 movePoint.position += new Vector3(PlayerController.direction, 0f, 0f);
+//                             }
+//                             break;
+//                         case 1:
+//                             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3((PlayerController.direction), 0f, 0f), .2f, borderCheck))
+//                             {
+//                                 movePoint.position += new Vector3(PlayerController.direction, 0f, 0f);
+//                             }
+//                             break;
+//                         case 2:
+//                             if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, (PlayerController.direction) / 2, 0f), .2f, borderCheck))
+//                             {
+//                                 movePoint.position += new Vector3(0f, (PlayerController.direction) / 2, 0f);
+//                             }
+//                             break;
+//                     }
+//                     break;
 
 
-                // Up Conveyor Tile Case
-                case 2:
-                    PlayerController.direction = = tileDirection;
-                    if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 1f, 0f), .2f, borderCheck))
-                    {
-                        PlayerController.inputCheck = false;
-                        movePoint.position += new Vector3(0f, 1f, 0f);
-                    }
-                    else
-                    {
-                        PlayerController.inputCheck = true;
-                    }
-                    break;
+//                 // Right Conveyor Tile Case
+//                 case 1:
+//                     PlayerController.direction = tileDirection;
+//                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(1f, 0f, 0f), .2f, borderCheck))
+//                     {
+//                         PlayerController.inputCheck = false;
+//                         movePoint.position += new Vector3(1f, 0f, 0f);
+//                     }
+//                     else
+//                     {
+//                         PlayerController.inputCheck = true;
+//                     }
+//                     break;
 
 
-                //Regular Tiles
-                case 3:
-                    PlayerController.inputCheck = true;
-                    break;
-            }
-        }
+//                 // Up Conveyor Tile Case
+//                 case 2:
+//                     PlayerController.direction = tileDirection;
+//                     if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, 1f, 0f), .2f, borderCheck))
+//                     {
+//                         PlayerController.inputCheck = false;
+//                         movePoint.position += new Vector3(0f, 1f, 0f);
+//                     }
+//                     else
+//                     {
+//                         PlayerController.inputCheck = true;
+//                     }
+//                     break;
 
 
-    }
-}
+//                 //Regular Tiles
+//                 case 3:
+//                     PlayerController.inputCheck = true;
+//                     break;
+//             }
+//         }
+
+
+//     }
+// }
